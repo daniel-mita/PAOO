a9b1a6e6263ab29eec298672717068dcff3a2423
Mita Daniel <daniel@Mitas-MacBook-Pro.local>
2022-10-20 18:43:03 +0300

    lab2

---
+++ _build/CMakeFiles/myapp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myapp.dir/exec.cpp.o"
+  "CMakeFiles/myapp.dir/exec.cpp.o.d"
+  "myapp"
+  "myapp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myapp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
